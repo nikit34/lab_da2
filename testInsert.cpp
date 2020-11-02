29d826dc32760a5e8ad943960ed4cdd20d417607
nikit34 <permikov134@main.ru>
2020-11-02 10:22:12 +0300

    fix testInsert

--- testInsert.cpp
+++ testInsert.cpp
@@ -22,20 +22,20 @@ int main(int argc, char** argv) {
 	int time = 0;
 	int map_time = 0;
 
+	start = std::chrono::system_clock::now();
 	for (int i = 0; i <= 1000000; ++i) {
-		start = std::chrono::system_clock::now();
 		t.Add(top + 1, top + 1);
-		end = std::chrono::system_clock::now();
-		time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-		t.Delete(top + 1);
-
-		// map
-		start = std::chrono::system_clock::now();
-		test_map.insert({top + 1, top + 1});
-		end = std::chrono::system_clock::now();
-		map_time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-		test_map.erase(top + 1);
 	}
+	end = std::chrono::system_clock::now();
+	time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+	start = std::chrono::system_clock::now();
+	for (int i = 0; i <= 1000000; ++i) {
+		test_map.insert({top + 1, top + 1});
+	}
+	end = std::chrono::system_clock::now();
+	map_time += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	
 	std::cout << "AVL TIME: " << time << "\nMAP TIME: " << map_time << std::endl;
 	return 0;
 }
